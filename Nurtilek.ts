eaf9f1972ac6062543f37ba47a671df915e3a8ef
Nurai <nooraselfeght@gmail.com>
2022-10-12 11:18:23 +0600

    added file

---
+++ Nurtilek.ts
@@ -0,0 +1,23 @@
+const objects = [
+  {
+    id: 'Object1',
+    name: 'Anna'
+  }, {
+    id: 'Object2',
+    age: 78
+  }, {
+    id: 'Object3',
+    hasHome: false,
+    kkkk: 'jjjj'
+  }
+];
+
+type ObjType = {
+  id: string
+}
+
+const mapFromArray = <T extends ObjType>(objects : T[]) => {
+  return objects.reduce((acc, i) => ({...acc, [i.id]: i}), {})
+}
+
+console.log(mapFromArray(objects));
